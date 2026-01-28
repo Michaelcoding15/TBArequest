75bdc76f2dde23cf1c5e96879478cc53751c2101
TigerQa <mickelbick@gmail.com>
2026-01-28 15:20:10 -0500

    different bundling accounted for

--- scripts/fix-dts.ts
+++ scripts/fix-dts.ts
@@ -3,17 +3,19 @@ import { readFileSync, writeFileSync } from "fs";
 const file = readFileSync("./dist/index.d.mts").toString();
 
 const lines = file.split("\n");
+
+const type = lines[0].slice(12, 20);
+
+const object =
+  lines[1][48] == " " ? lines[1].slice(12, 48) : lines[1].slice(12, 49);
+const array =
+  lines[2][47] == " " ? lines[2].slice(12, 47) : lines[2].slice(12, 48);
+
 lines.splice(1, 2);
 
 let text = lines.join("\n");
 
-text = text.replaceAll(
-  "arktype_internal_variants_array_ts0.ArrayType",
-  "arktype0.Type",
-);
-text = text.replaceAll(
-  "arktype_internal_variants_object_ts0.ObjectType",
-  "arktype0.Type",
-);
+text = text.replaceAll(`${array}.ArrayType`, `${type}.Type`);
+text = text.replaceAll(`${object}.ObjectType`, `${type}.Type`);
 
 writeFileSync("./dist/index.d.mts", text);
